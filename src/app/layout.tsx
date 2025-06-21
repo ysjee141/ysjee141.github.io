a3f0c938c466816103c6e13ac72ec0a8d7560bd9
Happ(ys.ji) <ysjee141@gmail.com>
2025-06-22 08:06:24 +0900

    feat: 애드센스 메타 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,13 +34,14 @@ export default function RootLayout({
   return (
     <html lang="en">
     <head>
+      <meta name="google-adsense-account" content="ca-pub-2570618898847074"/>
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2570618898847074"
               crossOrigin="anonymous"></script>
     </head>
     <body>
     <AntdRegistry>
       <Layout style={LayoutStyle}>
-        <SiteHeader/>
+      <SiteHeader/>
         <Content style={ContentStyle}>
           {children}
         </Content>
